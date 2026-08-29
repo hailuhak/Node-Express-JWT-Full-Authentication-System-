7a4187d948a2d5fccc5f30d991f99461018d3418
hailu <hailu@gmail.com>
2026-08-29 21:56:01 +0300

    feat: add user signup and login controllers

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,42 @@
+import { User } from "../Models/user.js";
+
 export const signup_get = (req, res) => {
     res.render("signup");
 };
+
 export const login_get = (req, res) => {
     res.render("login");
 };
-export const signup_post = (req, res) => {
- const { email, password } = req.body;
- console.log(email, password )
-    res.send("Signup received");
-};
-export const login_post = (req, res) => {
-     console.log(req.body);
 
-    res.send("Signup")
+export const signup_post = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.create({
+            email,
+            password
+        });
+
+        console.log(user);
+
+        res.send("Signup successful");
+    } catch (error) {
+        console.log(error);
+        res.status(400).send("Signup failed");
+    }
+};
+
+export const login_post = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        console.log(user);
+
+        res.send("Login received");
+    } catch (error) {
+        console.log(error);
+        res.status(400).send("Login failed");
+    }
 };
